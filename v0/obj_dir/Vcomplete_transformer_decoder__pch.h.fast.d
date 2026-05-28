25be3ad86b128ce19b21de4b4234f13a40522453
Nicholas Armstrong <nwa31104@gmail.com>
2026-05-28 03:07:36 -0400

    Untrack generated Verilator / Vivado / training artifacts

--- v0/obj_dir/Vcomplete_transformer_decoder__pch.h.fast.d
+++
@@ -1,9 +0,0 @@
-Vcomplete_transformer_decoder__pch.h.fast.gch: \
-  Vcomplete_transformer_decoder__pch.h \
-  /opt/homebrew/Cellar/verilator/5.046/share/verilator/include/verilated.h \
-  /opt/homebrew/Cellar/verilator/5.046/share/verilator/include/verilated_config.h \
-  /opt/homebrew/Cellar/verilator/5.046/share/verilator/include/verilatedos.h \
-  /opt/homebrew/Cellar/verilator/5.046/share/verilator/include/verilated_types.h \
-  /opt/homebrew/Cellar/verilator/5.046/share/verilator/include/verilated_funcs.h \
-  Vcomplete_transformer_decoder__Syms.h Vcomplete_transformer_decoder.h \
-  Vcomplete_transformer_decoder___024root.h
